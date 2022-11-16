c7e9e9cbd5f921ff4cddd7a49525669a7bc8ab12
Nicolas Cavallin <nicavallin@gmail.com>
2022-11-16 11:50:12 +0100

    iterators

--- src/iterators.rs
+++ src/iterators.rs
@@ -1,10 +1,9 @@
-
 pub fn iterators() {
-  println!("Welcome to Iterators");
+    println!("Welcome to Iterators");
 
-  let products = [1,2,3,4,5,6,7,8,9,10];
+    let products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  for x in products.iter(){
-    println!("Product: {}", x);
-  }
+    for x in products.iter() {
+        println!("Product: {}", x);
+    }
 }
