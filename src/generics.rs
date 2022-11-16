8c30af97ef03de05340dc28d142cbe9f56825c85
Nicolas Cavallin <nicavallin@gmail.com>
2022-11-16 10:36:48 +0100

    playing with generics

--- src/generics.rs
+++ src/generics.rs
@@ -1,4 +1,27 @@
-
 pub fn main() {
-  println!("Hello, generics!");
+    println!("Hello, generics!");
+    /*
+     * Option<T> -> Se define el T en tiempo de ejecución, no en compilación
+     */
+
+    let pointA: Point<i32> = Point { x: 1, y: 2 };
+    let pointB: Point<f32> = Point { x: 1.5, y: 2.5 };
+
+    // No es tan flexible, va a ser f o i para los dos valores, por ende tendría que usar X.0 para enteros, o presentar una solución como la siguiente
+
+    let pointC: PointFlex<i32, f32> = PointFlex { x: 1, y: 2.5 };
+
+    // También puedo NO definir los tipos
+
+    let pointD = PointFlex { x: "Hola", y: true };
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+struct PointFlex<T, V> {
+    x: T,
+    y: V,
 }
